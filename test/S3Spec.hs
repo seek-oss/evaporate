b5c68f94ce373c62b95bbc0594c8f860e2eb4c32
Adam Skoufis <askoufis@seek.com.au>
2016-12-13 11:21:49 +1100

    Rename tests after function name change

--- test/S3Spec.hs
+++ test/S3Spec.hs
@@ -10,43 +10,43 @@ main = hspec spec
 
 spec :: Spec
 spec = describe "S3" $ do
-  context "making alternate paths for folders" $ do
-    it "can make an alternate path for a non-nested folder, a non-nested file \
-       \and a non-nested alternate folder path" $ do
+  context "making s3 file paths for folders" $ do
+    it "can make an s3 file path for a non-nested folder, a non-nested file \
+       \and a non-nested s3 folder path" $ do
       let folderPath = "folder1"
       let filePath = "folder1/test.txt"
-      let altFolderPath = "myFolder"
-      let result = makeS3FilePath altFolderPath folderPath filePath
+      let s3FolderPath = "myFolder"
+      let result = makeS3FilePath s3FolderPath folderPath filePath
       result `shouldBe` "myFolder/test.txt"
 
-    it "can make an alternate path for a non-nested folder, a nested file and \
-       \a non-nested alternate folder path" $ do
+    it "can make an s3 file path for a non-nested folder, a nested file and \
+       \a non-nested s3 folder path" $ do
       let folderPath = "folder1"
       let filePath = "folder1/folder2/test.txt"
-      let altFolderPath = "myFolder"
-      let result = makeS3FilePath altFolderPath folderPath filePath
+      let s3FolderPath = "myFolder"
+      let result = makeS3FilePath s3FolderPath folderPath filePath
       result `shouldBe` "myFolder/folder2/test.txt"
 
-    it "can make an alternate path for a nested folder, a nested file and a \
-       \non-nested alternate folder path" $ do
+    it "can make an s3 file path for a nested folder, a nested file and a \
+       \non-nested s3 folder path" $ do
       let folderPath = "folder1/folder2"
       let filePath = "folder1/folder2/test.txt"
-      let altFolderPath = "myFolder"
-      let result = makeS3FilePath altFolderPath folderPath filePath
+      let s3FolderPath = "myFolder"
+      let result = makeS3FilePath s3FolderPath folderPath filePath
       result `shouldBe` "myFolder/test.txt"
 
-    it "can make an alternate path for a non-nested folder, a non-nested file \
-       \and a nested alternate folder path" $ do
+    it "can make an s3 file path for a non-nested folder, a non-nested file \
+       \and a nested s3 folder path" $ do
       let folderPath = "folder1"
       let filePath = "folder1/test.txt"
-      let altFolderPath = "myFolder/myInnerFolder"
-      let result = makeS3FilePath altFolderPath folderPath filePath
+      let s3FolderPath = "myFolder/myInnerFolder"
+      let result = makeS3FilePath s3FolderPath folderPath filePath
       result `shouldBe` "myFolder/myInnerFolder/test.txt"
 
-    it "can make an alternate path for a nested folder, a nested file and a \
-       \nested alternate folder path" $ do
+    it "can make an s3 file path for a nested folder, a nested file and a \
+       \nested s3 folder path" $ do
       let folderPath = "folder1/folder2"
       let filePath = "folder1/folder2/test.txt"
-      let altFolderPath = "myFolder/myInnerFolder"
-      let result = makeS3FilePath altFolderPath folderPath filePath
+      let s3FolderPath = "myFolder/myInnerFolder"
+      let result = makeS3FilePath s3FolderPath folderPath filePath
       result `shouldBe` "myFolder/myInnerFolder/test.txt"
